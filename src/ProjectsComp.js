da580797f5d05c1e6b95547503932abb2d9656df
mariahlusneac <mariahlusneac0@gmail.com>
2019-04-21 23:05:12 +0300

    code refactoring

--- src/ProjectsComp.js
+++ src/ProjectsComp.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import './proiect.css';
 
-export default class Layout extends Component {
+export default class ProjectsComp extends Component {
     render(){
+        
         return (
                 <div className="item3">
                     <table style={{width:'100%'}}>
                         <tr>
-                        <th className="tablehead2" colspan="2">Projects</th>
+                            <th className="tablehead2" colspan="2">Projects</th>
                         </tr>
                         <tr>
                             <th className="firstrow project">Proiect</th>
@@ -15,16 +16,16 @@ export default class Layout extends Component {
                                 <span className="secondcolumn">Due to:</span>
                                 <span className="duedate secondcolumn">5/17/2019</span>
                             </th>
-                            </tr>
-                            <tr>
-                                <th className="project">Proiect</th>
+                        </tr>
+                        <tr>
+                            <th className="project">Proiect</th>
                                 <th>
                                     <span className="secondcolumn">Due to:</span>
                                     <span className="duedate secondcolumn">10/17/2019</span>
                                 </th>
-                            </tr>
-                        </table>
-                    </div>
+                        </tr>
+                    </table>
+                </div>
         );
     }
 }
