e146015ed5759f697974dc4f9de5fdc4f152c219
LaurBalaur <burluc.laurentiu@gmail.com>
2019-04-29 20:09:44 +0300

    Component ViewPlan

---
+++ view-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import  ViewPlan  from './Components/ViewPlan.js';
+
+
+class App extends React.Component {
+  render() {
+    return(
+      <div className="App">
+        <ViewPlan />
+      </div>
+    );
+  }
+}
+
+export default App;
