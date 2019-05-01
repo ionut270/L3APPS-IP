38687ec509337729e67e17c0122c3e47cc76e2b2
LaurBalaur <burluc.laurentiu@gmail.com>
2019-05-01 12:14:32 +0300

    added progress button

--- view-app/src/App.js
+++ view-app/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import ViewPlan from "./Components/ViewPlan.js";
 import ViewTask from "./Components/ViewTask.js";
 import ViewSubTask from "./Components/ViewSubTask.js";
+import ProgressButton from "./Components/ProgressButton.js";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <ViewSubTask />
+        <ProgressButton />
       </div>
     );
   }
