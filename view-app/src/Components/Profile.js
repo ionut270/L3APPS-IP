18e281f7ab7270e132a6cf158fb258cfd8f2708f
LaurBalaur <burluc.laurentiu@gmail.com>
2019-05-10 20:29:42 +0300

    Am terminat comopnenta Profile Information

--- view-app/src/Components/Profile.js
+++ view-app/src/Components/Profile.js
@@ -12,6 +12,7 @@ import {
   Progress,
   Modal
 } from "semantic-ui-react";
+import ProgressButton from "./ProgressButton.js";
 const panes = [
   {
     menuItem: "Profile Information",
@@ -19,43 +20,22 @@ const panes = [
       <Tab.Pane>
         <Container textAlign="justified">
           <Grid>
-            <Grid.Column width={3}>
-              <Segment color="red" tertiary>
+            <Grid.Row>
+              <Grid.Column width={5} textAlign="center">
                 <Image
                   src={require("./avatar.jpg")}
                   alt="avatar"
                   size="small"
+                  centered
                   circular
                 />
-              </Segment>
+                <Segment color="red" secondary>
+                  <Header as="h4">Ongoing tasks:</Header>
+                  <Header as="h4">Done tasks:</Header>
+                </Segment>
+              </Grid.Column>
 
-              <Header as="h4" attached="top">
-                Job
-              </Header>
-              <Segment color="red" secondary attached>
-                <List divided relaxed>
-                  <List.Item>
-                    <List.Content>
-                      <List.Header>Job Name</List.Header>
-                      Nume aici
-                    </List.Content>
-                  </List.Item>
-                  <List.Item>
-                    <List.Content>
-                      <List.Header>Function</List.Header>
-                      Functie aici
-                    </List.Content>
-                  </List.Item>
-                  <List.Item>
-                    <List.Content>
-                      <List.Header>Category</List.Header>Categorie aici
-                    </List.Content>
-                  </List.Item>
-                </List>
-              </Segment>
-            </Grid.Column>
-            <Grid.Column width={13}>
-              <Segment color="red">
+              <Grid.Column width={11}>
                 <Divider hidden />
                 <Container textAlign="left">
                   <Header as="h4">Username :</Header>
@@ -63,8 +43,41 @@ const panes = [
                   <Header as="h4">UserID:</Header>
                   <Divider />
                   <Header as="h4">Email:</Header>
+                  <Divider />
+                  <Header as="h4">Profile Description:</Header>
                 </Container>
                 <Divider />
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={5}>
+                <Header as="h4" attached="top">
+                  Job
+                </Header>
+                <Segment color="red" secondary attached>
+                  <List divided relaxed>
+                    <List.Item>
+                      <List.Content>
+                        <List.Header>Job Name</List.Header>
+                        Nume aici
+                      </List.Content>
+                    </List.Item>
+                    <List.Item>
+                      <List.Content>
+                        <List.Header>Function</List.Header>
+                        Functie aici
+                      </List.Content>
+                    </List.Item>
+                    <List.Item>
+                      <List.Content>
+                        <List.Header>Category</List.Header>Categorie aici
+                      </List.Content>
+                    </List.Item>
+                  </List>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column width={11}>
+                {" "}
                 <Header as="h4" attached="top">
                   Difficulty
                 </Header>
@@ -90,8 +103,13 @@ const panes = [
                     </List.Item>
                   </List>
                 </Segment>
-              </Segment>
-            </Grid.Column>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={2}>
+                <ProgressButton />
+              </Grid.Column>
+            </Grid.Row>
           </Grid>
         </Container>
       </Tab.Pane>
