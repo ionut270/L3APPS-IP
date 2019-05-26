3951c90a3e94dcb278c934826d22cc827054baae
LaurBalaur <burluc.laurentiu@gmail.com>
2019-05-26 14:19:59 +0300

    Edit button

--- src/components/Dashboard/EditTask.jsx
+++ src/components/Dashboard/EditTask.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Divider, Segment, Icon, List, Button } from "semantic-ui-react";
+import { Divider, Segment, Icon, List, Button, Grid } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import MyHeader from "../Header";
 
@@ -8,7 +8,19 @@ class EditTask extends Component {
     return (
       <div className="EditTask">
         <MyHeader />
-        {this.props.location.state.id}
+
+        <div className="dashFlex">
+          <Grid columns="equal" className="DashboardGeneralSize">
+            <Grid.Column>
+              <Grid columns="equal">
+                <Grid.Row>
+                  {" "}
+                  <Segment color="red">Insereaza insereaza aici</Segment>
+                </Grid.Row>
+              </Grid>
+            </Grid.Column>
+          </Grid>
+        </div>
       </div>
     );
   }
