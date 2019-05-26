e4a52293cf73113e55bd1de12012f2538d83221a
LaurBalaur <burluc.laurentiu@gmail.com>
2019-05-26 14:28:04 +0300

    Revert "Added route for edit task"
    This reverts commit f0adf366f36f7bf535af0b975781d24e5077bd75.

--- src/components/Dashboard/My_tasks.jsx
+++ src/components/Dashboard/My_tasks.jsx
@@ -2,37 +2,30 @@ import React, { Component } from "react";
 import "semantic-ui-css/semantic.min.css";
 import MyHeader from "../Header";
 import PersonalTasks from "./PersonalTasks";
-
 import { Grid } from "semantic-ui-react";
 
 import "../../style/Dashboard/App.css";
 
 export default class My_tasks extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tasks: []
-    };
-  }
-  render() {
-    return (
-      <div className="dashContainer">
-        <MyHeader />
-        <div className="dashFlex">
-          <Grid columns="equal" className="DashboardGeneralSize">
-            {/* <Grid.Column width={8}>
+    render() {
+        return (
+            <div className="dashContainer">
+                <MyHeader />
+                <div className="dashFlex">
+                    <Grid columns="equal" className="DashboardGeneralSize">
+                        {/* <Grid.Column width={8}>
                             <PersonalTasks />
                         </Grid.Column> */}
-            <Grid.Column>
-              <Grid columns="equal">
-                <Grid.Row>
-                  <PersonalTasks />
-                </Grid.Row>
-              </Grid>
-            </Grid.Column>
-          </Grid>
-        </div>
-      </div>
-    );
-  }
+                        <Grid.Column>
+                            <Grid columns="equal">
+                                <Grid.Row>
+                                    <PersonalTasks />
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Column>
+                    </Grid>
+                </div>
+            </div>
+        );
+    }
 }
