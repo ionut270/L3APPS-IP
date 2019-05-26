13da6cbb50fe939fc422e943236d20630b4edc8a
ionut270 <oanceaionut15@gmai.com>
2019-05-27 00:52:44 +0300

    Route for / is now dashboard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import Login from "./components/Auth/LoginForm.jsx";
 import Register from "./components/Auth/SignupForm.jsx";
 import Dashboard from "./components/dashboard";
@@ -10,27 +10,27 @@ import EditTask from "./components/Dashboard/EditTask.js";
 import MyTasks from "./components/Dashboard/My_tasks.jsx";
 
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isAuth: true
-    };
-  }
-  render() {
-    console.log("State is ", this.state);
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/task" component={Task} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/My tasks" component={MyTasks} />
-          <Route path="/edittask" component={EditTask} />
-          {/* <Route path="/underling" component={Underlings} /> */}
-        </div>
-      </BrowserRouter>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			isAuth: true,
+		};
+	}
+	render() {
+		console.log("State is ", this.state);
+		return (
+			<BrowserRouter>
+				<Route exact path="/" render={() => <Redirect to="/dashboard" />} />
+				<div className="App">
+					<Route path="/login" component={Login} />
+					<Route path="/register" component={Register} />
+					<Route path="/dashboard" component={Dashboard} />
+					<Route path="/task" component={Task} />
+					<Route path="/profile" component={Profile} />
+					<Route path="/My tasks" component={MyTasks} />
+					<Route path="/edittask" component={EditTask} />
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
